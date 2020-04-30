3d4b0668700851eabd8f9802e83e3e1d37370b7e
Tanawit Kritwongwimian <tanawit139@gmail.com>
2020-05-01 01:25:00 +0700

    fix import module error

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from '../user/user.module';
 
 @Module({
   imports:[
     JwtModule.register({
       secret: 'parallel-chat'
-    })
+    }),
+    UserModule
   ],
   controllers: [AuthController],
   providers: [AuthService],
