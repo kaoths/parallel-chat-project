b8acf0cd5ee6a3b55b284f6a6d647dbfeeac2c24
Tanawit Kritwongwimian <tanawit139@gmail.com>
2020-04-30 15:32:18 +0700

    change indent

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -10,33 +10,34 @@ import { Socket, Server } from 'socket.io';
 
 @WebSocketGateway()
 export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-    @WebSocketServer() server: Server;
+  @WebSocketServer() server: Server;
 
-    @SubscribeMessage('toServer')
-    handleMessage(@ConnectedSocket() client: Socket, @MessageBody() payload: any): void {
-        this.server.emit('toClient', payload); //echo checker
-    }
+  @SubscribeMessage('toServer')
+  handleMessage(@ConnectedSocket() client: Socket, @MessageBody() payload: any): void {
+    this.server.emit('toClient', payload); //echo checker
+  }
 
-    @SubscribeMessage('joinRoom')
-    handleJoinRoom(@ConnectedSocket() client: Socket, @MessageBody() roomName: string): void {
-        client.join(roomName);
-        this.server.to(client.id).emit('joinedRoom', roomName);
-    }
-    @SubscribeMessage('toRoom')
-    handleMessageToRoom(@ConnectedSocket() client: Socket, @MessageBody() message: string){
-        const room = Object.keys(client.rooms).filter(room => room !== client.id);
-        this.server.to(room[0]).emit('toClient', message)
-    }
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(@ConnectedSocket() client: Socket, @MessageBody() roomName: string): void {
+    client.join(roomName);
+    this.server.to(client.id).emit('joinedRoom', roomName);
+  }
 
-    afterInit(server: Server) {
-        console.log('init', server);
-    }
+  @SubscribeMessage('toRoom')
+  handleMessageToRoom(@ConnectedSocket() client: Socket, @MessageBody() message: string) {
+    const room = Object.keys(client.rooms).filter(room => room !== client.id);
+    this.server.to(room[0]).emit('toClient', message);
+  }
 
-    handleDisconnect(client: Socket): any {
-        console.log(`${client.id} disconnected`);
-    }
+  afterInit(server: Server) {
+    console.log('init', server);
+  }
 
-    handleConnection(client: Socket, ...args): any {
-        console.log(`${client.id} connected`, args);
-    }
+  handleDisconnect(client: Socket): any {
+    console.log(`${client.id} disconnected`);
+  }
+
+  handleConnection(client: Socket, ...args): any {
+    console.log(`${client.id} connected`, args);
+  }
 }
