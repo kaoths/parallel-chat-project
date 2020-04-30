4053248eb1395a0c33ac7a03529b4f12d1618de9
UtopiaBeam <utopiabeam@gmail.com>
2020-04-30 22:09:54 +0700

    :fire: Remove unused import

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -3,11 +3,9 @@ import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { Chat } from './chat.model';
-import { ChatController } from './chat.controller';
 
 @Module({
   imports: [TypegooseModule.forFeature([Chat])],
-  controllers: [ChatController],
   providers: [ChatGateway, ChatService],
 })
 export class ChatModule {}
